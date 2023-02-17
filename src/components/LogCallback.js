6fc75ce156f57c8ebd3c55fe6934a7c8a492458f
Ryan <ryanpvoong@gmail.com>
2023-02-16 20:58:52 -0500

    added logs to check callback code

--- src/components/LogCallback.js
+++ src/components/LogCallback.js
@@ -9,7 +9,7 @@ function LogCallback() {
   React.useEffect(() => {
     const serverUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_DEPLOYED_SERVER : process.env.REACT_APP_SERVER_BASEURL
     const code = `${location.search.split('=')[1]}`;
-    // console.log(code);
+    console.log(code);
 
     async function retrieveMalToken() {
       const malToken = await fetch(`${ serverUrl }/mal-auth?code=${ code }`, { credentials: 'include' })
